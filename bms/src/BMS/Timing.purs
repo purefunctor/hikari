3e9d4b0ff28ec36e8d765c85d0b68b8896dbff4d
PureFunctor <purefunctor@gmail.com>
2022-03-14 17:55:50 +0800

    Update documentation

--- bms/src/BMS/Timing.purs
+++ bms/src/BMS/Timing.purs
@@ -1,3 +1,4 @@
+-- | Functions for computing the timings of measures and notes.
 module BMS.Timing where
 
 import Prelude
@@ -14,9 +15,9 @@ import Data.Semigroup.Foldable (foldl1)
 import Data.Tuple.Nested (type (/\))
 import Data.Unfoldable (class Unfoldable)
 
--- | Compute the starting positions of a measure's notes (in seconds)
--- | given the `bpm` and the measures `start` position (in seconds).
--- | Notes with the same timing are joined together to form a chord.
+-- | Computes the starting positions of a measure's notes given the
+-- | `bpm` and the `start` position of the measure. Notes starting
+-- | at the same time are joined together to form a chord.
 noteTimings :: Number -> Number -> Measure -> Map Number (NonEmpty List Note)
 noteTimings bpm start (Measure { factor, notes }) =
   let
@@ -56,8 +57,9 @@ noteTimings'
   -> f (Number /\ NonEmpty List Note)
 noteTimings' bpm start = Map.toUnfoldable <<< noteTimings bpm start
 
--- | Compute the starting positions (in seconds) of measures given the
--- | `bpm`.
+-- | Computes the starting positions of a non-empty list of measures
+-- | given the `bpm`. Note that this function assumes that the given
+-- | list of measures is ordered and is not missing an index.
 measureTimings :: Number -> NonEmpty List Measure -> Map Measure Number
 measureTimings bpm (m :| ms) = Map.fromFoldable (zip measures timings)
   where
@@ -84,9 +86,12 @@ measureTimings'
   -> f (Measure /\ Number)
 measureTimings' bpm = Map.toUnfoldable <<< measureTimings bpm
 
--- | Compute all starting positions (in seconds) of all notes across
--- | all measures given the `bpm`. Notes with the same timing are
--- | joined together to form a chord.
+
+-- | Computes the starting positions of all notes across a non-empty
+-- | list of measures given the `bpm`. Notes starting at the same time
+-- | are joined together to form a chord. Similarly to
+-- | `measureTimings`, this function also assumes that the list of
+-- | measures is ordered and has no missing indices.
 scoreTimings :: Number -> NonEmpty List Measure -> Map Number (NonEmpty List Note)
 scoreTimings bpm = foldlWithIndex go Map.empty <<< measureTimings bpm
   where
