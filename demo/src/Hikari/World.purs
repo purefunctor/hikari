a0a10a70ed74fc2e19ed54e5d6cc9896531fd348
PureFunctor <purefunctor@gmail.com>
2022-03-21 10:39:20 +0800

    Initial scaffolding for app

--- demo/src/Hikari/World.purs
+++ demo/src/Hikari/World.purs
@@ -1,13 +1,38 @@
 module Hikari.World where
 
-import BMS.Timing (NoteOffsets)
+import Prelude
+
+import BMS.Timing (Headers, NoteOffsets)
 import BMS.Types (Note)
+import Control.Parallel.Class (parallel, sequential)
 import Data.Map (Map)
-import WAGS.WebAPI (BrowserAudioBuffer)
+import Data.Map as Map
+import Data.String.Common (split)
+import Data.String.Pattern (Pattern(..))
+import Data.TraversableWithIndex (traverseWithIndex)
+import Data.Tuple.Nested ((/\))
+import Effect.Aff (Aff)
+import Effect.Exception.Unsafe (unsafeThrow)
+import WAGS.Interpret (decodeAudioDataFromUri)
+import WAGS.WebAPI (AudioContext, BrowserAudioBuffer)
 
 type KeySoundBuffers = Map Note BrowserAudioBuffer
 
 type World =
   { keySoundBuffers :: KeySoundBuffers
-  , noteOffsets :: NoteOffsets
+  , noteWithOffsets :: NoteOffsets
   }
+
+loadKeySoundBuffers
+  :: forall headers
+   . AudioContext
+  -> { headers :: Headers | headers }
+  -> Aff KeySoundBuffers
+loadKeySoundBuffers audioCtx { headers: { wavs } } = map Map.fromFoldable $ sequential $
+  traverseWithIndex go wavs
+  where
+  go note fileName = case split (Pattern ".") fileName of
+    [ fileName', _ ] -> parallel do
+      (note /\ _) <$> decodeAudioDataFromUri audioCtx ("./sounds/" <> fileName' <> ".ogg")
+    _ ->
+      unsafeThrow $ "Could not load file: " <> fileName
--- demo/src/Main.purs
+++ demo/src/Main.purs
@@ -2,65 +2,34 @@ module Main where
 
 import Prelude
 
+import BMS.Parser (bms)
+import BMS.Timing (gatherAll, noteOffsets)
 import Effect (Effect)
-
--- import BMS.Convert (convertNoteInstructions, findWavs)
--- import BMS.Parser (headers, measures)
--- import BMS.Timing (scoreTimings')
--- import BMS.Types (Note(..))
--- import Control.Parallel (parallel, sequential)
--- import Data.Either (fromRight)
--- import Data.List (List(..))
--- import Data.Map as Map
--- import Data.Maybe (fromMaybe)
--- import Data.String (split)
--- import Data.String.Pattern (Pattern(..))
--- import Data.Traversable (for)
--- import Data.Tuple.Nested ((/\))
--- import Effect (Effect)
--- import Effect.Aff (launchAff_)
--- import Effect.Class (liftEffect)
--- import Effect.Class.Console (log, logShow)
--- import Effect.Exception.Unsafe (unsafeThrow)
--- import FRP.Event (subscribe)
--- import FRP.Event.Time as Time
--- import Hikari.Engine (makePiece)
--- import Hikari.Fetch (fetchText)
--- import Hikari.Oracle (oracle)
--- import Hikari.Setup (setup)
--- import Hikari.Types (Residuals)
--- import Text.Parsing.StringParser (runParser)
--- import WAGS.Interpret (context, decodeAudioDataFromUri, makeFFIAudioSnapshot)
--- import WAGS.Run (TriggeredRun, runNoLoop)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import FRP.Event (subscribe)
+import FRP.Event.Time (interval)
+import Hikari.Engine (scene)
+import Hikari.Fetch (fetchText)
+import Hikari.Residuals (Residuals)
+import Hikari.World (loadKeySoundBuffers)
+import WAGS.Interpret (context, makeFFIAudioSnapshot)
+import WAGS.Run (TriggeredRun, runNoLoop)
 
 main :: Effect Unit
-main = pure unit
+main = launchAff_ do
+  audioCtx <- liftEffect context
+  ffiAudio <- liftEffect $ makeFFIAudioSnapshot audioCtx
 
--- main :: Effect Unit
--- main = launchAff_ do
---   audioCtx <- liftEffect context
---   ffiAudio <- liftEffect $ makeFFIAudioSnapshot audioCtx
+  bme <- gatherAll <<< bms <$> fetchText "./sounds/01.bme"
+  let noteWithOffsets = noteOffsets bme
+  keySoundBuffers <- loadKeySoundBuffers audioCtx bme
 
---   headers <- fromRight Nil <<< runParser headers <$> fetchText "./sounds/headers.txt"
---   noteSounds <- map Map.fromFoldable $ sequential $ for (findWavs headers)
---     \(Note note /\ soundName) ->
---       case split (Pattern ".") soundName of
---         [ soundName', _ ] -> parallel do
---           log $ "Loading: " <> soundName'
---           (note /\ _) <$> decodeAudioDataFromUri audioCtx ("./sounds/" <> soundName' <> ".ogg")
---         _ ->
---           unsafeThrow $ "invalid file name: " <> soundName
+  let
+    timeEvent = interval 1000 $> unit
+    world = { keySoundBuffers, noteWithOffsets }
 
---   log $ "Finished loading " <> show (Map.size noteSounds) <> " note sounds."
+  _ <- liftEffect $ subscribe (runNoLoop timeEvent (pure world) {} ffiAudio scene)
+    (\(_ :: TriggeredRun Residuals ()) -> pure unit)
 
---   notes <- fromRight Nil <<< runParser measures <$> fetchText "./sounds/notes.txt"
---   let noteTimings = fromMaybe Nil $ convertNoteInstructions notes <#> scoreTimings' 105.0
---   logShow noteTimings
-
---   let timeEvent = Time.interval 10 $> unit
-
---   _ <- liftEffect $
---     subscribe (runNoLoop timeEvent (pure { noteSounds, noteTimings }) {} ffiAudio (makePiece setup oracle))
---       (\(_ :: TriggeredRun Residuals ()) -> pure unit)
-
---   pure unit
+  pure unit
